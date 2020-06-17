109fb698c3bdbbe20a1d37f2b421c9308e31931b
Bill <william.hine2019@gmail.com>
2020-06-17 12:23:32 -0400

    Missed Save From Last Commit

--- TrashCollector/Startup.cs
+++ TrashCollector/Startup.cs
@@ -12,6 +12,9 @@ using TrashCollector.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using TrashCollector.ActionFilters;
 
 namespace TrashCollector
 {
@@ -36,6 +39,14 @@ namespace TrashCollector
             .AddDefaultUI()
             .AddDefaultTokenProviders();
             services.AddControllersWithViews();
+
+            services.AddScoped<ClaimsPrincipal>(s =>
+            s.GetService<IHttpContextAccessor>().HttpContext.User);
+            services.AddControllers(config =>
+            {
+                config.Filters.Add(typeof(GlobalRouting));
+            });
+
             services.AddRazorPages();
         }
 
