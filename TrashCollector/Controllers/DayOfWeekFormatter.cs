2a89d756511b6440dd88c4bca001b3dfdd0fc508
Bill <william.hine2019@gmail.com>
2020-06-24 14:27:43 -0400

    Added Logic To Capitalize The Day Of The Week

---
+++ TrashCollector/Controllers/DayOfWeekFormatter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector.Controllers
+{
+    public class DayOfWeekFormatter
+    {
+
+        public static string FormatDay(string toformat)
+        {
+            string allLower = toformat.ToLower();
+            string firstCapital = char.ToUpper(allLower[0]).ToString();
+
+            return firstCapital; 
+        }
+    }
+}
